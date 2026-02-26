26ce9508fe077b77d84c941adb29cb84cede7289
Peter Schwenk <paschwenk@gmail.com>
2026-02-26 17:12:17 -0500

    After "Connect" lesson

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,15 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const connectionString = "amqp://guest:guest@localhost:5672/";
+  const connection = await amqp.connect(connectionString);
+  console.log("Connected to RabbitMQ");
+
+  process.on("SIGINT", async () => {
+    console.log("Closing RabbitMQ connection...");
+    await connection.close();
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
