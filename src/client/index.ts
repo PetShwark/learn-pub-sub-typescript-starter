edbdf6f21848bf7c6a3b1605075a25744cc36660
Peter Schwenk <paschwenk@gmail.com>
2026-03-08 15:54:59 -0400

    After "Client REPL" lesson

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,22 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
-import { declareAndBind, SimpleQueueType } from "../internal/pubsub/declare_bind.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import {
+  clientWelcome,
+  getInput,
+  commandStatus,
+  printClientHelp,
+  printQuit
+} from "../internal/gamelogic/gamelogic.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
+import {
+  declareAndBind,
+  SimpleQueueType
+} from "../internal/pubsub/declare_bind.js";
+import {
+  ExchangePerilDirect,
+  PauseKey
+} from "../internal/routing/routing.js";
 
 
 async function main() {
@@ -16,6 +31,37 @@ async function main() {
     PauseKey,
     SimpleQueueType.Transient
   );
+  const gameState = new GameState(userName);
+  // REPL loop
+  while (true) {
+    const userInput = await getInput();
+    if (userInput.length > 0) {
+      switch (userInput[0]) {
+        case "spawn":
+          commandSpawn(gameState, userInput);
+          break;
+        case "move":
+          commandMove(gameState, userInput);
+          break;
+        case "status":
+          commandStatus(gameState);
+          break;
+        case "help":
+          printClientHelp();
+          break;
+        case "quit":
+          printQuit();
+          process.exit(0);
+          break;
+        case "spam":
+          console.log("Spamming not allowed yet!.");
+          break;
+        default:
+          console.log(`Command ${userInput[0]} unknown.`);
+          break;
+      }
+    }
+  }
 }
 
 main().catch((err) => {
